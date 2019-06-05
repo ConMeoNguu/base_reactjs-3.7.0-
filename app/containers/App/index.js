3fe9e15e282a2fd724a732f323613c94a9a0c75a
Miêu Hắc <dtulovepthuy@gmail.com>
2019-06-05 15:58:24 +0700

    update

--- app/containers/App/index.js
+++ app/containers/App/index.js
@@ -21,11 +21,10 @@ import HomePage from '../HomePage';
 import GlobalStyle from '../../global-styles';
 
 const AppWrapper = styled.div`
-  max-width: calc(768px + 16px * 2);
+  width: 100%;
   margin: 0 auto;
   display: flex;
   min-height: 100%;
-  padding: 0 16px;
   flex-direction: column;
 `;
 
